8b35a3d5debec390b44aa7e5bba82efa15feb571
Augustin <augustin.yvon@laplateforme.io>
2024-01-08 14:17:13 +0100

    job-03

---
+++ job-03/Category.php
@@ -0,0 +1,78 @@
+<?php
+class Category
+{
+    private int $id;
+    private string $name;
+    private string $description;
+    private DateTime $createdAt;
+    private DateTime $updatedAt;
+
+    public function __construct(
+        int $id,
+        string $name,
+        string $description,
+        DateTime $createdAt,
+        DateTime $updatedAt
+    ) {
+        $this->id = $id;
+        $this->name = $name;
+        $this->description = $description;
+        $this->createdAt = $createdAt;
+        $this->updatedAt = $updatedAt;
+    }
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getName(): string
+    {
+       return $this->name;
+    }
+
+    public function getDescription(): string
+    {
+       return $this->description;
+    }
+
+    public function getCreatedAt(): DateTime
+    {
+       return $this->createdAt;
+    }
+
+    public function getUpdatedAt(): DateTime
+    {
+       return $this->updatedAt;
+    }
+
+    public function setId(int $id): Category
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    public function setName(string $name): Category
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    public function setDescription(string $description): Category
+    {
+        $this->description = $description;
+        return $this;
+    }
+
+    public function setCreatedAt(DateTime $createdAt): Category
+    {
+        $this->createdAt = $createdAt;
+        return $this;
+    }
+
+    public function setUpdatedAt(DateTime $updatedAt): Category
+    {
+        $this->updatedAt = $updatedAt;
+        return $this;
+    }
+}
---
+++ job-03/Product.php
@@ -0,0 +1,135 @@
+<?php
+class Product
+{
+    private int $id;
+    private string $name;
+    private array $photos;
+    private float $price;
+    private string $description;
+    private int $quantity;
+    private DateTime $createdAt;
+    private DateTime $updatedAt;
+    private int $category_id;
+
+    public function __construct(
+        int $id = 0,
+        string $name = '',
+        array $photos = [],
+        float $price = 0.0,
+        string $description = '',
+        int $quantity = 0,
+        DateTime $createdAt = null,
+        DateTime $updatedAt = null,
+        int $category_id = 0
+    )
+    {
+        $this->id = $id;
+        $this->name = $name;
+        $this->photos = $photos;
+        $this->price = $price;
+        $this->description = $description;
+        $this->quantity = $quantity;
+        $this->createdAt = $createdAt;
+        $this->updatedAt = $updatedAt;
+        $this->category_id = $category_id;
+    }
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function getPhotos(): array
+    {
+        return $this->photos;
+    }
+
+    public function getPrice(): float
+    {
+        return $this->price;
+    }
+
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+
+    public function getQuantity(): int
+    {
+        return $this->quantity;
+    }
+
+    public function getCreatedAt(): DateTime
+    {
+        return $this->createdAt;
+    }
+
+    public function getUpdatedAt(): DateTime
+    {
+        return $this->updatedAt;
+    }
+
+    public function getCategory_id(): int
+    {
+        return $this->category_id;
+    }
+
+    public function setId(int $id): Product
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    public function setName(string $name): Product
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    public function setPhotos(array $photos): Product
+    {
+        $this->photos = $photos;
+        return $this;
+    }
+
+    public function setPrice(float $price): Product
+    {
+        $this->price = $price;
+        return $this;
+    }
+
+    public function setDescription(string $description): Product
+    {
+        $this->description = $description;
+        return $this;
+    }
+
+    public function setQuantity(int $quantity): Product
+    {
+        $this->quantity = $quantity;
+        return $this;
+    }
+
+    public function setCreatedAt(DateTime $createdAt): Product
+    {
+        $this->createdAt = $createdAt;
+        return $this;
+    }
+
+    public function setUpdatedAt(DateTime $updatedAt): Product
+    {
+        $this->updatedAt = $updatedAt;
+        return $this;
+    }
+
+    public function setCategory_id(int $category_id): Product
+    {
+        $this->category_id = $category_id;
+        return $this;
+    }
+}
